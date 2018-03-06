d128e2067eb7007d077fb29554a88e9fd493ce3d
Oliver Galt <gonca2372@gmail.com>
2018-03-06 16:48:06 +0000

    Typedefs

--- src/Estruturas/Bloco.h
+++ src/Estruturas/Bloco.h
@@ -7,4 +7,20 @@ typedef struct utilizador *Util ;
 typedef struct post * Post;
 
 
+typedef struct utilizador {
+
+	unsigned char nome[100];
+	unsigned int Q;
+	unsigned int A;
+
+}*Util ;
+
+typedef struct post {
+
+	unsigned int id;
+    unsigned int fundador;
+	unsigned char nome[100];
+
+}*Post;
+
 #endif
