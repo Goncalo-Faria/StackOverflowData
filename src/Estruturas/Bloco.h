8ed0f31bc344c725886d9c5b4717181d4d7e8b93
GuilhermeViveiros <a80524@alunos.uminho.pt>
2018-03-06 15:15:54 +0000

    Blocos

---
+++ src/Estruturas/Bloco.h
@@ -0,0 +1,22 @@
+#ifndef bloco_h
+#define bloco_h
+
+#define Max 100
+
+	typedef struct hash_1 {
+
+		char nome[100];
+		char fundador[100];
+
+	}*HASH1;
+
+	typedef struct hash_2 {
+
+		int perguntas;
+		int respostas;
+		char nome[100];
+
+	}*HASH2;
+
+
+#endif
