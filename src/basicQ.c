d46edfd6b641994989a48748f4dfab859f762184
GuilhermeViveiros <a80524@alunos.uminho.pt>
2018-03-08 18:27:42 +0000

    basicQ with more functions

--- src/basicQ.c
+++ src/basicQ.c
@@ -20,9 +20,8 @@ STR_pair info_from_post(TAD_community com, int id);
 LONG_list top_most_active(TAD_community com, int N);
 
 
-// Métodos privados-->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-static void f (void* key, void* value, void* user_data){
+// Métodos privados-->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+static void target_freq (void* key, void* value, void* user_data){
     Util x = (Util)value;
     HEAP y = (HEAP)user_data;
     int  num = x->Q + x->A;
@@ -33,7 +32,7 @@ static void f (void* key, void* value, void* user_data){
         add_Heap( y , (-1) * num , value );
 }
 
-static void g ( void* key , void *value , void *datas ) {
+static void target_int( void* key , void *value , void *datas ) {
 
     Date x = (Date) key ;
     Contain y = (Contain) datas
@@ -48,9 +47,29 @@ static void g ( void* key , void *value , void *datas ) {
         else y->a++;
     }
 }
+/*
+// REPARAR -> GONCAS
+static void target_ans( void *key , void*value , void* hp ){
+
+    Heap heap = (Heap) hp;
+
+    Contain data = (Contain) key;
+
+    Post post = (Post) value;
+
+    // falta implementar
+    int num = p -> score ;
+    if ( data_compare ( Contain -> dateB ) <= 0 && data_compare ( Contain -> dateE ) >= 0 && post -> type == 1 ) {
+     if ( maxQ_H (heap) )// se está na capacidade
+            addR_Heap( data , - 1 * (num)  , post  , destroyPost );
+        else 
+            add_Heap( data , (-1) * num , post );
+
+}
+*/
 
 //->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
+// -- 1 FEITO
 STR_pair info_from_post(TAD_community com, int id){
     char* str1, *str2;
     Util y = NULL;
@@ -77,6 +96,7 @@ STR_pair info_from_post(TAD_community com, int id){
     return result;
 }
 
+// --2 FEITO
 LONG_list top_most_active(TAD_community com, int N){
     int num;
 
@@ -101,14 +121,14 @@ LONG_list top_most_active(TAD_community com, int N){
 // recebe uma avl tree e retira de la as datas , para um su-array defenido no glib
 // estou a assumir que recebo uma AVL;
 
-
+// --3 FEITO
 LONG_pair total_posts(TAD community com, Date begin, Date end) {
 
     Contain x = g_malloc (sizeof (struct contain));
     x->dateB = begin;
     x->dateE = end; 
 
-    // nao esta defenido por incompetencia (LONG_PAI) xD !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // nao esta defenido por incompetencia (LONG_PAIR) xD !!!!!!!!!!!!!!!!!!!!!!!!!!!!
     x->q=0;
     x->a=0;
 
@@ -121,29 +141,59 @@ LONG_pair total_posts(TAD community com, Date begin, Date end) {
     return res;
 }
 
+// --4 precisamos da tag
+LONG_list questions_with_tag(TAD community com, char* tag, Date begin, Date end) {
 
 
+}
 
 
-/*
-
+// --5 ESTA POR ACABAR
 USER get_user_info(TAD community com, long id){
 
+USER create_user(char* short_bio, long* post_history);
+
+    Heap x = limcreate_H ( )
     User x  = NULL;
     util y = NULL;
 
-    // preciso que o id seja do tipo int
-    y = (Util)g_hash_table_lookup(com->util ,& (int *)id);
-    // atraves do ID da me isso
+    y = (Util)g_hash_table_lookup(com->util ,id);
 
     // create_user (char* short_bio, long* post_history);
-
-
-   
     x = create_user ( x -> bio , x -> Q) ;
     g_free(y);
 
     return x;
 }
 
-*/
+ 
+// --6 FEITA
+LONG_list most_voted_answers(TAD community com, int N, Date begin, Date end){
+     int num;
+     LONG_list x = create_list(N);
+
+     Heap h = limcreate_H (N);
+     g_tree_foreach (com->treeP , n , (void *)x);
+
+     for (int i=0, i < N ; i++){
+        post = (Post) rem_Heap( x , &num );
+        set_list(x, i , post);
+     }
+
+
+}
+
+// --7 FALTA ACABAR
+LONG_list most_answered_questions(TAD community com, int N, Date begin, Date end){
+
+    
+
+
+
+}
+
+
+
+
+
+
