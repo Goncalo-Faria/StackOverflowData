739ada7c1f55e8dff0f7c595f7d0d3a7cba49582
GuilhermeViveiros <a80524@alunos.uminho.pt>
2018-03-08 20:51:30 +0000

    Funcoes que faltavam do AVL.C

--- include/AVL.h
+++ include/AVL.h
@@ -1,4 +1,4 @@
-//#include "date.h"
+#include "date.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,17 +6,52 @@
 #define AVL_h
 
 
-
+// struct da tentry , recebe como parametros uma key e dados
 typedef struct tentry* TreeEntry;
 
+/*
+*balanceador da minha AVL :
+*LH -> Left 
+*EH -> Equilibrado
+*RH -> Direita
+*/
 typedef enum balancefactor { LH , EH , RH } BalanceFactor;
 
+//struct do Tree
 typedef struct treenode *Tree;
 
+//struct da AVL
 typedef struct avl *AVL;
 
+/*
+*Insere um nodo na Tree
+*tem um elemento cresceu caso a arvore aumente de tamanho
+*/
 Tree insertTree(Tree t, TreeEntry e, int *cresceu);
 
+// cria a AVL
+/*
+*compare ..
+..
+*/
+AVL create_avl ( int (*compare) (void* , void*, void* ), void (*free_key) (void *), void (*free_data) (void*) );
+
+//insere na AVL uma determinada key e dados (TreeEntry)
+AVL insertAVL (AVL a , void *key , void *dados);
+
+//aplica uma rotacao da Tree ha esquerda
+Tree rotateLeft(Tree t);
+
+//aplica uma rotacao da Tree ha direita
+Tree rotateRight(Tree t);
+
+//Balanceia a arvore ha direita
+Tree balanceRight(Tree t);
+
+//Balanceia a arvore ha esquerda
+Tree balanceLeft(Tree t)
+
+//verifica se é AVL (tipo booleano)
 int isAVL (Tree t);
 
 
