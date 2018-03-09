d69e2b30f96521194aa089707685474d67961874
GuilhermeViveiros <a80524@alunos.uminho.pt>
2018-03-09 22:19:27 +0000

    novo atributo para questao 7

--- src/Bloco.c
+++ src/Bloco.c
@@ -2,17 +2,15 @@
 //#include <stdlib.h>
 #include <string.h>
 #include <interface.h>
-#include <glib.h>
-//#include "date.h"
 
 
 typedef struct utilizador {
 
 	//unsigned long id;
-	unsigned char *nome;
+	unsigned char nome[100];
 	unsigned int Q;
 	unsigned int A;
-	unsigned char *bio;
+	unsigned char bio [200];
 	GHashTable* bacia;
 //	unsigned int AGE;
 //	unsigned char loc[100];
@@ -23,13 +21,14 @@ typedef struct utilizador {
 
 typedef struct post {
 
-	unsigned int id;
+	//unsigned int id;
+	unsigned int answers;
 	unsigned char type;// 1 Q ; 2 A;
     unsigned long fundador;
-	unsigned char *nome;
+	unsigned char nome[100];
 	unsigned int score;
 
-	Date moment;
+	//Date data;
 
 }*Post;
 
@@ -43,21 +42,20 @@ static void null_check ( void * x ){
 // Métodos publicos.
 
 void *createPost(){
-	Post x  = g_malloc (sizeof (struct post));
-	x->nome = NULL;
+	Post x = g_malloc (sizeof (struct post));
+	strcpy((char*)x->nome,"Nop");
 	x->type = 0;
 	x->fundador = 0;
 	x->score = 0;
-	x->id = 0;
+	x->answers = 0;
 	//x->data = g_malloc (sizeof(struct date));
-	x->moment = createDate ( 0 , 0 , 0 );
+	//x->data = createDate ( 0 , 0 , 0 );
 	return x;
 }
 
 void destroyPost( void* x ){
 	Post y = (Post) x;
-	free_date(y->moment);
-	null_check(y->nome);
+	free_date(y->data) ;
 	g_free(y);
 }
 
@@ -65,8 +63,8 @@ void destroyPost( void* x ){
 
 void *createUtil(){
 	Util x = g_malloc (sizeof (struct utilizador));
-	x->nome =NULL;
-	x->bio =NULL;
+	strcpy((char*)x->nome,"Nop");
+	strcpy((char*)x->bio, "Nop");
 	x->Q = 0;
 	x->A = 0;
 
@@ -79,14 +77,27 @@ void *createUtil(){
 void destroyUtil( void* x ){
 	Util y = (Util) x;
 
-	null_check(y->nome);
-	null_check(y->bio);
-
-	g_hash_table_destroy(y->bacia);
+	g_hash_table_destroy(x->bacia);
 	g_free(y);
 }
 
+/*
+void destroyUtil_key( void* x ){
+	Util y = (Util) x;
+	free(y);
+}
+*/
+
+void setDate ( Post x ,int d,int m ,int a){
+	free_date(x->date);
+	x->date = createDate(d,m,a);
+}
+
 // Util getters
+
+GHashTable* get_Bacia(Util x){
+	return (x->bacia);
+}
 
 unsigned int getU_Q(Util x) {
 	return (x->Q);
@@ -96,50 +107,25 @@ unsigned int getU_A(Util x) {
 	return (x->A);
 }
 
-unsigned char* getU_name(Util x){
-	// must eliminate
-	unsigned char* tmp=NULL;
-
-	if(x->nome ){
-
-		tmp = g_malloc(sizeof(unsigned char)*(strlen((const char*) x->nome ) + 1 )); 
-		strcpy((char*)tmp, (const char* )x->nome);
-	}
-	return tmp ;
+char* getU_name(Util x){
+	return (x->nome);
 }
 
-unsigned char* getU_bio(Util x){
-	unsigned char* tmp=NULL;
-
-	if(x->bio){
-
-		tmp = g_malloc(sizeof(unsigned char)*(strlen((const char*) x->bio ) + 1 )); 
-		strcpy(( char*)tmp,(const char*) x->bio);
-	}
-	return tmp ;
+char* getU_bio(Util x){
+	return (x->bio);
 }
 
 // Post getters
-unsigned int getP_id(Post x){
-	return ( x->id );
-}
 
 unsigned long getP_fund(Post x){
 	return (x->fundador);
 }
 
-unsigned char* getP_name(Post x){
-	unsigned char* tmp=NULL;
-
-	if(x->nome){
-
-		tmp = g_malloc(sizeof(unsigned char)*(strlen( (const char*)x->nome ) + 1 )); 
-		strcpy(( char*)tmp,(const char*) x->nome);
-	}
-	return tmp ;
+char* getP_name(Post x){
+	return (x->nome);
 }
 
-unsigned int getP_score(Post x){
+unsigned int getP_Score(Post x){
 	return (x->score);
 }
 
@@ -147,86 +133,36 @@ unsigned char getP_type(Post x){
 	return (x->type);
 }
 
-Date getP_date_point( Post x ){
-	// memória dinâmica.
-	//return createDate( get_day(x->moment), get_month(x->moment), get_year(x->moment) ) ; 
-	return x->moment;
+unsigned int getP_answers(Post x){
+	return (x->answers);
 }
-Date getP_date( Post x ){
-	// memória dinâmica.
-	return createDate( get_day(x->moment), get_month(x->moment), get_year(x->moment) ) ; 
-}
+
 //Util setters
 
-void inc_Q(Util x){
+void incQ(Util x){
 	x->Q++;
 }
 
-void inc_A(Util x){
+void incA(Util x){
 	x->A++;
 }
 
-void setU_name(Util x, const unsigned char* un ){
-	
-	null_check(x->nome);
-	x->nome =  g_malloc(sizeof( unsigned char)*(strlen((const char*)un ) + 1 )); 
-	strcpy(( char*)x->nome,(const char*) un);
+void setU_name(Util x, char* un){
+	strcpy(x->nome, un);
 }
 
-void setU_bio(Util x, const unsigned char* un){
-	null_check(x->bio);
-	x->bio =  g_malloc(sizeof(unsigned char)*(strlen((const char*) un ) + 1 )); 
-	strcpy(( char*)x->bio,(const char*) un);
-}
-
-void add_toBacia(Util x, unsigned int * id , void * dados ){
-    
-    g_hash_table_insert( x->bacia , (void*) id , dados );
-}
-
-int belongs_toBacia ( Util x , unsigned int Parent_id, char flag ){ // O que acontece na eventualidade de 2 ter uma resposta no post que criou? em datas diferentes.
-    
-	GHashTable* set = x->bacia;
-	// flag == 1; meens Q.
-    // flag == 2; meens A.
-    if( flag == 1 && g_hash_table_contains ( set , &Parent_id ) )
-        // é uma questão e existe.
-        return 1;
-
-    if( flag == 2 && g_hash_table_contains ( set , &Parent_id  ) ){
-		// é uma Resposta.
-        if ( g_hash_table_lookup(set, &Parent_id) )
-            //isto significa que existe questão.
-            return 1;
-		return 2;
-		}
-
-    return 0;
-
-
+void setU_bio(Util x, char* b){
+	strcpy(x->bio, b);
 }
 
 // Post setters
-
-void setP_id(Post x, unsigned int o ){
-	x->id = o;
-}
-
-void setP_date( Post x , int d, int m , int a ){
-	
-	free_date(x->moment);
-	x->moment = createDate(d,m,a);
-}
 
 void setP_fund(Post x, long f){
 	x->fundador = f;
 }
 
-void setP_name(Post x, const unsigned char* un){
-
-	null_check(x->nome);
-	x->nome = g_malloc(sizeof( unsigned char)*(strlen( (const char*) un ) + 1 )); 
-	strcpy(( char*)x->nome, (const char*)un);
+void setP_name(Post x, char* n){
+	strncpy(x->nome, n);
 }
 
 void setP_score(Post x, unsigned int s){
@@ -235,6 +171,10 @@ void setP_score(Post x, unsigned int s){
 
 void setP_type(Post x, unsigned char t){
 	x->type = t;
+}
+
+void setP_answers(Post x , unsigned int a){
+	x->asnwers = a;
 }
 
 
