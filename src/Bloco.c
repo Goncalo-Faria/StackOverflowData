439541fb8e733b12bd212f1dfdacac711177a72f
Oliver Galt <gonca2372@gmail.com>
2018-03-09 15:25:18 +0000

    minor bug fixes

---
+++ src/Bloco.c
@@ -0,0 +1,257 @@
+#include "Bloco.h"
+//#include <stdlib.h>
+#include <string.h>
+#include <interface.h>
+#include <glib.h>
+//#include "date.h"
+
+
+typedef struct utilizador {
+
+	//unsigned long id;
+	unsigned char *nome;
+	unsigned int Q;
+	unsigned int A;
+	unsigned char *bio;
+	GHashTable* bacia;
+//	unsigned int AGE;
+//	unsigned char loc[100];
+//	unsigned int UV;
+//	unsigned int DV;
+
+}*Util ;
+
+typedef struct post {
+
+	unsigned int id;
+	unsigned char type;// 1 Q ; 2 A;
+    unsigned long fundador;
+	unsigned char *nome;
+	unsigned int score;
+
+	Date moment;
+
+}*Post;
+
+// Métodos privados.
+
+static void null_check ( void * x ){
+	if ( x )
+		g_free(x);
+}
+
+// Métodos publicos.
+
+void *createPost(){
+	Post x  = g_malloc (sizeof (struct post));
+	x->nome = NULL;
+	x->type = 0;
+	x->fundador = 0;
+	x->score = 0;
+	x->id = 0;
+	//x->data = g_malloc (sizeof(struct date));
+	x->moment = createDate ( 0 , 0 , 0 );
+	return x;
+}
+
+void destroyPost( void* x ){
+	Post y = (Post) x;
+	free_date(y->moment);
+	null_check(y->nome);
+	g_free(y);
+}
+
+// create -- destroy -- getters (P e respostas)
+
+void *createUtil(){
+	Util x = g_malloc (sizeof (struct utilizador));
+	x->nome =NULL;
+	x->bio =NULL;
+	x->Q = 0;
+	x->A = 0;
+
+	x->bacia = g_hash_table_new_full(g_int_hash ,  g_int_equal, g_free , null_check );// key é post.
+
+	
+	return x;
+}
+
+void destroyUtil( void* x ){
+	Util y = (Util) x;
+
+	null_check(y->nome);
+	null_check(y->bio);
+
+	g_hash_table_destroy(y->bacia);
+	g_free(y);
+}
+
+/*
+void destroyUtil_key( void* x ){
+	Util y = (Util) x;
+	free(y);
+}
+*/
+/*
+void setDate ( Post x ,int d,int m ,int a){
+	free_date(x->date);
+	x->date = createDate(d,m,a);
+}
+*/
+
+// Util getters
+
+unsigned int getU_Q(Util x) {
+	return (x->Q);
+}
+
+unsigned int getU_A(Util x) {
+	return (x->A);
+}
+
+unsigned char* getU_name(Util x){
+	// must eliminate
+	unsigned char* tmp=NULL;
+
+	if(x->nome ){
+
+		tmp = g_malloc(sizeof(unsigned char)*(strlen((const char*) x->nome ) + 1 )); 
+		strcpy((char*)tmp, (const char* )x->nome);
+	}
+	return tmp ;
+}
+
+unsigned char* getU_bio(Util x){
+	unsigned char* tmp=NULL;
+
+	if(x->bio){
+
+		tmp = g_malloc(sizeof(unsigned char)*(strlen((const char*) x->bio ) + 1 )); 
+		strcpy(( char*)tmp,(const char*) x->bio);
+	}
+	return tmp ;
+}
+
+// Post getters
+unsigned int getP_id(Post x){
+	return ( x->id );
+}
+
+unsigned long getP_fund(Post x){
+	return (x->fundador);
+}
+
+unsigned char* getP_name(Post x){
+	unsigned char* tmp=NULL;
+
+	if(x->nome){
+
+		tmp = g_malloc(sizeof(unsigned char)*(strlen( (const char*)x->nome ) + 1 )); 
+		strcpy(( char*)tmp,(const char*) x->nome);
+	}
+	return tmp ;
+}
+
+unsigned int getP_score(Post x){
+	return (x->score);
+}
+
+unsigned char getP_type(Post x){
+	return (x->type);
+}
+
+Date getP_date_point( Post x ){
+	// memória dinâmica.
+	//return createDate( get_day(x->moment), get_month(x->moment), get_year(x->moment) ) ; 
+	return x->moment;
+}
+Date getP_date( Post x ){
+	// memória dinâmica.
+	return createDate( get_day(x->moment), get_month(x->moment), get_year(x->moment) ) ; 
+}
+//Util setters
+
+void inc_Q(Util x){
+	x->Q++;
+}
+
+void inc_A(Util x){
+	x->A++;
+}
+
+void setU_name(Util x, const unsigned char* un ){
+	
+	null_check(x->nome);
+	x->nome =  g_malloc(sizeof( unsigned char)*(strlen((const char*)un ) + 1 )); 
+	strcpy(( char*)x->nome,(const char*) un);
+}
+
+void setU_bio(Util x, const unsigned char* un){
+	null_check(x->bio);
+	x->bio =  g_malloc(sizeof(unsigned char)*(strlen((const char*) un ) + 1 )); 
+	strcpy(( char*)x->bio,(const char*) un);
+}
+
+void add_toBacia(Util x, unsigned int * id , void * dados ){
+    
+    g_hash_table_insert( x->bacia , (void*) id , dados );
+}
+
+int belongs_toBacia ( Util x , unsigned int Parent_id, char flag ){ // O que acontece na eventualidade de 2 ter uma resposta no post que criou? em datas diferentes.
+    
+	GHashTable* set = x->bacia;
+	// flag == 1; meens Q.
+    // flag == 2; meens A.
+    if( flag == 1 && g_hash_table_contains ( set , &Parent_id ) )
+        // é uma questão e existe.
+        return 1;
+
+    if( flag == 2 && g_hash_table_contains ( set , &Parent_id  ) ){
+		// é uma Resposta.
+        if ( g_hash_table_lookup(set, &Parent_id) )
+            //isto significa que existe questão.
+            return 1;
+		return 2;
+		}
+
+    return 0;
+
+
+}
+
+// Post setters
+
+void setP_id(Post x, unsigned int o ){
+	x->id = o;
+}
+
+void setP_date( Post x , int d, int m , int a ){
+	
+	free_date(x->moment);
+	x->moment = createDate(d,m,a);
+}
+
+void setP_fund(Post x, long f){
+	x->fundador = f;
+}
+
+void setP_name(Post x, const unsigned char* un){
+
+	null_check(x->nome);
+	x->nome =  g_malloc(sizeof( unsigned char)*(strlen( (const char*) un ) + 1 )); 
+	strcpy(( char*)x->nome, (const char*)un);
+}
+
+void setP_score(Post x, unsigned int s){
+	x->score = s;
+}
+
+void setP_type(Post x, unsigned char t){
+	x->type = t;
+}
+
+
+
+
+
+
---
+++ src/heap.c
@@ -0,0 +1,179 @@
+//#include <stdlib.h>
+#include "heap.h"
+#include <glib.h>
+
+#define full(x) ( (x)->use == (x)->len )
+#define quarter(x) ( ( (x)->use * 4 ) <= (x)->len )
+
+typedef void (*freeFunc)(void*);
+
+typedef struct ent {
+    
+    int key;
+    void * data;
+
+} *ENTRY ;
+
+typedef struct heap {
+    
+    ENTRY * v;
+    int len;
+    int use;
+    int max;
+
+    freeFunc dataCl;
+
+} * HEAP;
+
+
+
+int length_H(HEAP x){
+    return x->use;
+}
+
+int maxQ_H(HEAP x){
+    return ( x->use == x->max );
+}
+
+HEAP create_H(void (*in_free) (void*) ){
+    HEAP x = (HEAP)g_malloc( sizeof (struct heap) );
+    x->use=0;
+    x->v = g_malloc( 2 * sizeof (ENTRY) );
+    x->len = 2;
+    x->max = -1;
+    x->dataCl = in_free;
+
+    return x;
+}
+
+HEAP limcreate_H(int lim, void (*in_free) (void*) ){
+    HEAP x = (HEAP)g_malloc( sizeof (struct heap) );
+    x->use=0;
+    x->v = g_malloc( 2 * sizeof (ENTRY) );
+    x->len = 2;
+    x->max = lim;
+    x->dataCl = in_free;
+    
+    return x;
+}
+
+void destroy_H(HEAP x){
+
+    freeFunc ff = x->dataCl;
+
+    int i,r = !(ff == NULL);
+    if(x){
+        for(i=0; i < x->use; i++ ){
+            if(x->v[i]->data && r)
+                ff(x->v[i]->data);
+            g_free(x->v[i]);
+        }
+        g_free( x->v );
+        g_free( x );
+    }
+
+}
+
+static void tabledouble(HEAP x ){
+
+    x->len *= 2;
+    x->v = g_realloc(x->v, x->len * sizeof ( ENTRY )  );
+
+}
+
+static void tablehalv(HEAP x){
+
+    x->len = x->len / 2;
+    x->v = g_realloc(x->v, x->len * sizeof ( ENTRY )  );
+
+}
+
+static void Swap (ENTRY *v, int i , int j){
+		
+	ENTRY t;
+    
+	// t = v[i];
+    //
+    t  = v[i];
+   
+    //
+	//v[i] = v[ j ];
+    //
+    v[i]= v[j];
+   
+    //
+	// v[ j ] = t;
+    //
+    v[j]  = t ;
+
+    
+} 
+
+static void BubleUp (ENTRY * v , int i ){
+
+	if( ! i )
+		return;
+
+	if(v[i]->key < v[ pai(i) ]->key){
+		Swap(v, i , pai(i) );
+		BubleUp(v , pai(i) );
+	}
+
+}
+
+static void BubleDown (ENTRY * v , int i, int N ){
+
+	int f = esq(i); 
+	if(f > N-1 )
+		return ;
+
+    if ( f + 1 < N )
+		f = v[f]->key < v[f+1]->key ? f : f+1 ;
+
+	if( v[f]->key < v[i]->key ){
+	    Swap (v,f,i ) ;
+	    BubleDown (v , f , N ) ;	
+	}
+
+}
+
+void addR_Heap( HEAP x, int key , void* n ){
+    
+    freeFunc ff = x->dataCl;
+
+    if ( ff )
+        ff ( x->v[x->use-1]->data );
+
+    x->v[x->use-1]->data = n;
+    x->v[x->use-1]->key  = key;
+
+    BubleUp(x->v , x->use-1 );
+}
+
+void add_Heap( HEAP x, int key , void* n ){
+
+    if( full(x) ) tabledouble(x);
+    x->v[x->use] = g_malloc( sizeof(struct ent) );
+    x->v[x->use]->data = n;
+    x->v[x->use++]->key = key;
+
+    BubleUp(x->v , x->use-1 );
+}
+
+void* rem_Heap( HEAP x, int *key){
+
+    void *n;
+
+    if ( quarter(x) ) tablehalv(x);
+
+    *key = x->v[0]->key ;
+    n    = x->v[0]->data;
+
+    x->v[0]->data = NULL;
+    
+    Swap(x->v,0, --x->use);
+    g_free(x->v[x->use]);
+
+    BubleDown(x->v , 0 , x->use);
+    return n;
+}
