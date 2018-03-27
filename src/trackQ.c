4299cd8fb3246407465ad4d0fb73a66678adbe3d
Oliver Galt <gonca2372@gmail.com>
2018-03-27 15:46:52 +0100

    q11 está a funcionar na perfeição

--- src/trackQ.c
+++ src/trackQ.c
@@ -8,11 +8,13 @@
 #include <comondec.h>
 
 // Métodos publicos.
-LONG_list most_used_best_rep(TAD_community com, int N, Date begin, Date end); //#11
+
 LONG_list top_most_active(TAD_community com, int N);                          //#2
 USER get_user_info(TAD_community com, long id);                               //#5
+LONG_list most_used_best_rep(TAD_community com, int N, Date begin, Date end); //#11
 
 // Métodos privados.
+
 static void get_active(void *key, void *value, void *user_data);
 static void collect_top10(void *key, void *value, void *user_data);
 static void gather_rep(void *key, void *value, void *user_data);
@@ -197,7 +199,7 @@ LONG_list top_most_active(TAD_community com, int N)
         {
             //printf(" || %d \n",(int)i+1);
             c = rem_Heap(hp);
-            set_list(ll, i , (long)getU_id(c));
+            set_list(ll, i, (long)getU_id(c));
         }
 
         for (i = j; i < N; j++)
@@ -209,91 +211,6 @@ LONG_list top_most_active(TAD_community com, int N)
     return ll;
 }
 
-LONG_list most_used_best_rep(TAD_community com, int N, Date begin, Date end)
-{
-    HEAP hp;
-    GHashTable *htable_usr, *htable_tag;
-    Util cur;
-    bArray extreme;
-    Record rd;
-    LONG_list ll;
-    char flag;
-    int i,j;
-    long code;
-
-    if (is_ON(com))
-    {
-        flag = 0;
-
-        rd = userSet_id_transversal(com, gather_rep, (void *)createRecord(init_A((unsigned long)N, NULL), &flag));
-
-        htable_usr = g_hash_table_new_full(g_int64_hash, g_int64_equal, NULL, NULL);
-        htable_tag = g_hash_table_new_full(g_int_hash, g_int_equal, NULL, NULL);
-
-        g_free(rd);
-
-        if (flag)
-        {
-            hp = (HEAP)rd->fst;
-        }
-        else
-        {
-            extreme = (bArray)rd->fst;
-            hp = Generalized_Priority_Queue(extreme, length_A(extreme), post_compare, yes, NULL);
-            destroy_A(extreme);
-        }
-
-        while (!empty_H(hp))
-        {
-            cur = (Util)rem_Heap(hp);
-            g_hash_table_insert(htable_usr, getU_id_point(cur), cur);
-        }
-
-        destroy_H(hp);
-
-        rd = arraySeg_transversal(com, begin, end, fil_hash, createRecord(htable_usr, htable_tag));
-
-        g_free(rd);
-        g_hash_table_destroy(htable_usr);
-
-        // > ha um erro a gerir memória.
-        flag = 0;
-        rd = createRecord(init_A(N, NULL), &flag);
-        g_hash_table_foreach(htable_tag, make_histogram, rd);
-        if (flag)
-        { // tudo na heap.
-            hp = (HEAP)rd->fst;
-        }
-        else
-        {
-            extreme = (bArray)rd->fst;
-            hp = Generalized_Priority_Queue(extreme, length_A(extreme), post_compare, yes, NULL);
-            destroy_A(extreme);
-        }
-
-        g_hash_table_destroy(htable_tag);
-        g_free(rd);
-
-        ll = create_list(N);
-
-        j = length_H(hp);
-        for (i = j - 1; i >= 0; i--)
-        {
-            rd = (Record)rem_Heap(hp);
-            code = (long)rd->fst;
-            set_list(ll, i, (long)code);
-            tag_count_free(rd);
-        }
-
-        for (i = j; i < N; i++)
-            set_list(ll, i, 0);
-
-        return ll;
-    }
-    else
-        return NULL;
-}
-
 USER get_user_info(TAD_community com, long id)
 {
     char flag;
@@ -302,7 +219,7 @@ USER get_user_info(TAD_community com, long id)
 
     long post_history[10];
     char *short_bio = NULL;
-    int i,j;
+    int i, j;
 
     Post the_post;
     USER send;
@@ -352,7 +269,7 @@ USER get_user_info(TAD_community com, long id)
         }
         //->>>>
         j = length_H(hp);
-        for (i = j-1 ; i>=0 ; i--)
+        for (i = j - 1; i >= 0; i--)
         { // vai do novo para o velho. (cronologia inversa)
 
             the_post = (Post)rem_Heap(hp);
@@ -372,6 +289,92 @@ USER get_user_info(TAD_community com, long id)
         destroy_H(hp);
 
         return send;
+    }
+    else
+        return NULL;
+}
+
+LONG_list most_used_best_rep(TAD_community com, int N, Date begin, Date end)
+{
+    HEAP hp;
+    GHashTable *htable_usr, *htable_tag;
+    Util cur;
+    bArray extreme;
+    Record rd;
+    LONG_list ll;
+    char flag;
+    int i, j;
+    int * code;
+
+    if (is_ON(com))
+    {
+        flag = 0;
+
+        rd = userSet_id_transversal(com, gather_rep, (void *)createRecord(init_A((unsigned long)N, NULL), &flag));
+
+        htable_usr = g_hash_table_new_full(g_int64_hash, g_int64_equal, NULL, NULL);
+        htable_tag = g_hash_table_new_full(g_int_hash, g_int_equal, NULL, NULL);
+
+        if (flag)
+        {
+            hp = (HEAP)rd->fst;
+        }
+        else
+        {
+            extreme = (bArray)rd->fst;
+            hp = Generalized_Priority_Queue(extreme, length_A(extreme), rep_cmp, yes, NULL);
+            destroy_A(extreme);
+        }
+        g_free(rd);
+
+        while (!empty_H(hp))
+        {
+            cur = (Util)rem_Heap(hp);
+            g_hash_table_insert(htable_usr, getU_id_point(cur), cur);
+        }
+
+        destroy_H(hp);
+
+        rd = arraySeg_transversal(com, begin, end, fil_hash, createRecord(htable_usr, htable_tag));
+
+        htable_usr = (GHashTable *)rd->fst;
+        htable_tag = (GHashTable *)rd->snd;
+
+        g_free(rd);
+        g_hash_table_destroy(htable_usr);
+
+        flag = 0;
+        rd = createRecord(init_A(N, NULL), &flag);
+        g_hash_table_foreach( htable_tag, make_histogram, rd);
+        if (flag)
+        { // tudo na heap.
+            hp = (HEAP)rd->fst;
+        }
+        else
+        {
+            extreme = (bArray)rd->fst;
+            hp = Generalized_Priority_Queue(extreme, length_A(extreme), tag_count_cmp, yes, NULL);
+            destroy_A(extreme);
+        }
+
+        g_hash_table_destroy(htable_tag);
+        g_free(rd);
+
+        ll = create_list(N);
+
+        j = length_H(hp);
+        for (i = j-1; i >=0 ; i-- )
+        {
+            rd = (Record)rem_Heap(hp);
+            code = (int*)rd->fst;
+            set_list(ll, i,(long) *code);
+            tag_count_free(rd);
+        }
+
+        for (i = j; i < N; i++)
+            set_list(ll, i, 0);
+
+        return ll;
     }
     else
         return NULL;
