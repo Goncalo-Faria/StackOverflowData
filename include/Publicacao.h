55c568c86d2545eb9a267803edb3f83e4ad84f4e
GuilhermeViveiros <a80524@alunos.uminho.pt>
2018-03-28 01:42:52 +0100

    more

--- include/Publicacao.h
+++ include/Publicacao.h
@@ -17,37 +17,6 @@ void *postAnswer_transversal(Post x, void *(*p)(Post, void *), void *a);
 //
 void *postTag_transversal(Post x, void (*p)(unsigned int, void *), void *a);
 
-//Post getters ---------------------------------------------------------------
-
-//devolve o Id de um post
-unsigned int getP_id(Post x);
-//devolve um apontador para o Id de um post
-unsigned int *getP_id_point(Post x);
-//devolve o ParentId de um post de um post
-unsigned int getP_parentId(Post x);
-//devolve o apontador para o ParentID de um post
-unsigned int *getP_parentId_point(Post x);
-//devolve o fundador de um post
-unsigned long getP_fund(Post x);
-//devolve o nome de um post
-unsigned char *getP_name(Post x);
-//devolve o apontador para o nome de um post
-unsigned char *getP_name_point(Post x);
-//
-unsigned int getP_ansCount(Post x);
-//devolve o score de um post
-unsigned int getP_score(Post x);
-//devolve o numero de votos de um post
-int getP_votes(Post x);
-//devolve o tipo de um post
-unsigned char getP_type(Post x);
-//devolve o numero de respostas de um post
-unsigned int getP_nComment(Post x);
-//devolve o apontador da data de um post
-Date getP_date_point(Post x);
-//devolve a data de um post
-Date getP_date(Post x);
-
 //Post setters ---------------------------------------------------------------
 
 Post setP_id(Post x, unsigned int o);
@@ -79,3 +48,35 @@ Post setP_score(Post x, unsigned int s);
 Post setP_type(Post x, unsigned char t);
 //coloca o id num post e devolve o post
 #endif
+
+//Post getters ---------------------------------------------------------------
+
+//devolve o Id de um post
+unsigned int getP_id(Post x);
+//devolve um apontador para o Id de um post
+unsigned int *getP_id_point(Post x);
+//devolve o ParentId de um post de um post
+unsigned int getP_parentId(Post x);
+//devolve o apontador para o ParentID de um post
+unsigned int *getP_parentId_point(Post x);
+//devolve o fundador de um post
+unsigned long getP_fund(Post x);
+//devolve o nome de um post
+unsigned char *getP_name(Post x);
+//devolve o apontador para o nome de um post
+unsigned char *getP_name_point(Post x);
+//
+unsigned int getP_ansCount(Post x);
+//devolve o score de um post
+unsigned int getP_score(Post x);
+//devolve o numero de votos de um post
+int getP_votes(Post x);
+//devolve o tipo de um post
+unsigned char getP_type(Post x);
+//devolve o numero de respostas de um post
+unsigned int getP_nComment(Post x);
+//devolve o apontador da data de um post
+Date getP_date_point(Post x);
+//devolve a data de um post
+Date getP_date(Post x);
+
