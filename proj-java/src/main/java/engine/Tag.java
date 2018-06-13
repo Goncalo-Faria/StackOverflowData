dbcb7f186ef8f68df7993ad901540bd9e9b64362
Goncalo-Faria <gonca2372@gmail.com>
2018-06-13 01:15:54 +0100

    doc

--- proj-java/src/main/java/engine/Tag.java
+++ proj-java/src/main/java/engine/Tag.java
@@ -9,8 +9,6 @@ public class Tag {
     private String id;
     private String nome;
 
-    /** ADD HASH CODE */
-
     static Set<String> obtainTagNames(String str) {
         HashSet<String> conj = new HashSet<String>();
         int count = 0;
@@ -29,45 +27,70 @@ public class Tag {
         return conj;
     }
 
+    /**
+     * Construtor vazio de Tag.
+     */
     public Tag() {
         this.id = null;
         this.nome = "nenhum";
     }
-
+    /**
+     * Construtor parametrisado de Tag.
+     */
     public Tag(String x, String y) {
         this.id = x;
         this.nome = y;
     }
 
+    /**
+     * Construtor de cópia de Tag.
+     */
     public Tag(Tag x) {
         this.id = x.getId();
         this.nome = x.getNome();
     }
 
     // Getters!----------------------------------------------------------------------------------------------------------------
+    /**
+     * Obtêm o idenficador.
+     */
     public String getId() {
         return this.id;
     }
 
+    /**
+     * Obtêm o nome.
+     */
     public String getNome() {
         return this.nome;
     }
 
     // Setters!----------------------------------------------------------------------------------------------------------------
+    /**
+     * Atribui um identicador.
+     */
     public void setId(String x) {
         this.id = x;
     }
 
+    /**
+     * Atribui um nome.
+     */
     public void setNome(String x) {
         this.nome = x;
     }
 
     // ---------------------------------------------------------------------------------------------------------------------------
-
+    /**
+     * Obtêm uma cópia deste objeto.
+     */
     public Tag clone() {
         return new Tag(this);
     }
 
+    /**
+     * Verifica a igualdade entre objetos.
+     */
     public boolean equals(Object x) {
         if (this == x)
             return true;
